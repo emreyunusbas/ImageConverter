eb05851665a9e9a26c40742eb31a69f025e9e747
emreyunus <emrebasyunus1@gmail.com>
2023-07-11 20:55:06 +0300

    Code Updated

--- ImageConverterUI.java
+++ ImageConverterUI.java
@@ -1,82 +1,108 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
 
 public class ImageConverterUI extends JFrame {
-    private JLabel label;
+
+    private JLabel statusLabel;
     private JButton selectButton;
     private JButton convertButton;
+
+    private JFileChooser fileChooser;
     private File[] selectedFiles;
+    private File outputFolder;
 
     public ImageConverterUI() {
-        setTitle("Image Converter");
-        setSize(400, 200);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
+        super("WebP to JPG Converter");
 
-        label = new JLabel("No files selected.");
-        label.setHorizontalAlignment(JLabel.CENTER);
+        setLayout(new FlowLayout());
 
+        statusLabel = new JLabel("Select WebP files and choose output folder.");
         selectButton = new JButton("Select Files");
+        convertButton = new JButton("Convert");
+
+        add(statusLabel);
+        add(selectButton);
+        add(convertButton);
+
+        fileChooser = new JFileChooser();
+        fileChooser.setMultiSelectionEnabled(true);
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        fileChooser.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("WebP Files", "webp"));
+
         selectButton.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
-                JFileChooser fileChooser = new JFileChooser();
-                fileChooser.setMultiSelectionEnabled(true);
-                int result = fileChooser.showOpenDialog(null);
+                int result = fileChooser.showOpenDialog(ImageConverterUI.this);
                 if (result == JFileChooser.APPROVE_OPTION) {
                     selectedFiles = fileChooser.getSelectedFiles();
-                    label.setText(selectedFiles.length + " files selected.");
+                    statusLabel.setText(selectedFiles.length + " files selected.");
                 }
             }
         });
 
-        convertButton = new JButton("Convert and Save");
         convertButton.addActionListener(new ActionListener() {
+            @Override
             public void actionPerformed(ActionEvent e) {
                 if (selectedFiles == null || selectedFiles.length == 0) {
-                    JOptionPane.showMessageDialog(null, "Please select files to convert.");
+                    JOptionPane.showMessageDialog(ImageConverterUI.this, "No files selected!", "Error", JOptionPane.ERROR_MESSAGE);
                     return;
                 }
 
-                JFileChooser fileChooser = new JFileChooser();
-                fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-                int result = fileChooser.showSaveDialog(null);
-                if (result == JFileChooser.APPROVE_OPTION) {
-                    File selectedDirectory = fileChooser.getSelectedFile();
-                    for (File file : selectedFiles) {
-                        try {
-                            String newFilePath = selectedDirectory.getAbsolutePath() + File.separator + file.getName().replace(".webp", ".jpg");
-                            Path sourcePath = file.toPath();
-                            Path destinationPath = new File(newFilePath).toPath();
-                            Files.copy(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
-                        } catch (IOException ex) {
-                            ex.printStackTrace();
-                        }
-                    }
-                    JOptionPane.showMessageDialog(null, "Conversion completed successfully!");
+                JFileChooser outputChooser = new JFileChooser();
+                outputChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                int outputResult = outputChooser.showDialog(ImageConverterUI.this, "Select Output Folder");
+                if (outputResult == JFileChooser.APPROVE_OPTION) {
+                    outputFolder = outputChooser.getSelectedFile();
+                    convertFiles();
                 }
             }
         });
 
-        setLayout(new BorderLayout());
-        JPanel panel = new JPanel(new GridLayout(3, 1));
-        panel.add(label);
-        panel.add(selectButton);
-        panel.add(convertButton);
-        add(panel, BorderLayout.CENTER);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 150);
+        setLocationRelativeTo(null);
+        setResizable(false);
+    }
+
+    private void convertFiles() {
+        for (File file : selectedFiles) {
+            String outputFilePath = outputFolder.getPath() + File.separator + getFileNameWithoutExtension(file) + ".jpg";
+            convertFile(file, outputFilePath);
+        }
+
+        JOptionPane.showMessageDialog(this, "Conversion completed!", "Success", JOptionPane.INFORMATION_MESSAGE);
+        statusLabel.setText("Select WebP files and choose output folder.");
+    }
+
+    private void convertFile(File inputFile, String outputFilePath) {
+        try {
+            BufferedImage webpImage = ImageIO.read(inputFile);
+            File outputFile = new File(outputFilePath);
+            ImageIO.write(webpImage, "jpg", outputFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getFileNameWithoutExtension(File file) {
+        String fileName = file.getName();
+        int lastDotIndex = fileName.lastIndexOf(".");
+        if (lastDotIndex != -1) {
+            return fileName.substring(0, lastDotIndex);
+        }
+        return fileName;
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                ImageConverterUI converterUI = new ImageConverterUI();
-                converterUI.setVisible(true);
+                new ImageConverterUI().setVisible(true);
             }
         });
     }
