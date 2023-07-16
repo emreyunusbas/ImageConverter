f705ec77233bf38abb771c2378c227104a9a2b0e
emreyunus <emrebasyunus1@gmail.com>
2023-07-16 23:15:38 +0300

    Update ImageConverterUI.java

--- ImageConverterUI.java
+++ ImageConverterUI.java
@@ -5,7 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
+import java.io.IOException; 
 
 public class ImageConverterUI extends JFrame {
 
